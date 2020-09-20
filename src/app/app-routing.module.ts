eb1eaaab6df39ad63eb53f6adae8120ed232d640
Filippo <Filippo.maccarellimh@gmail.com>
2020-09-20 22:27:05 +0200

    - ADD animations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { SearchUserComponent } from './pages/search-user/search-user.component';
 import { UserDetailsComponent } from './pages/user-details/user-details.component';
 
 const routes: Routes = [
-  { path: '', component: SearchUserComponent },
-  { path: 'user-details', component: UserDetailsComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'home', component: SearchUserComponent, data: {animation: 'Home'}},
+  { path: 'user-details', component: UserDetailsComponent, data: {animation: 'Details'}},
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { slideInAnimation } from './route-animation';
 import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [ slideInAnimation ]
 })
 export class AppComponent {
 
